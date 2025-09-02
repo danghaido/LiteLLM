29716059eb1da73e472b5faa6cf9714ad56fa4d1
danghaido <dohaidang1104@gmail.com>
2025-09-02 21:41:34 +0700

    Update on CSV

--- tools/export_csv.py
+++ tools/export_csv.py
@@ -24,6 +24,7 @@ os.environ["HUGGINGFACE_API_KEY"] = CONFIG.api_key
 phoenix_client = px.Client()
 span_client = Client()
 
+
 # Define query for spans with CHAIN kind
 def export_evaluation_to_csv():
     span_query = (
@@ -43,26 +44,22 @@ def export_evaluation_to_csv():
 
     # Get additional spans dataframe
     all_spans_df = span_client.spans.get_spans_dataframe(
-        query=SpanQuery(),
-        project_identifier=PROJECT_NAME
+        query=SpanQuery(), project_identifier=PROJECT_NAME
     )
 
     # Get span annotations dataframe
     annotations_df = span_client.spans.get_span_annotations_dataframe(
-        spans_dataframe=all_spans_df,
-        project_identifier=PROJECT_NAME
+        spans_dataframe=all_spans_df, project_identifier=PROJECT_NAME
     )
 
     # Process annotations dataframe
-    annotations_df = (
-        annotations_df
-        .reset_index()  # Convert index to column
-        .rename(columns={
+    annotations_df = annotations_df.reset_index().rename(  # Convert index to column
+        columns={
             "span_id": "context.span_id",
             "result.label": "eval",
             "result.explanation": "explanation",
             "annotation_name": "metrics",
-        })
+        }
     )
 
     # Merge spans and annotations dataframes
@@ -71,13 +68,14 @@ def export_evaluation_to_csv():
     # Define columns to keep
     selected_columns = [
         "context.span_id",
-        "input", 
+        "input",
         "output",
         "metrics",
         "eval",
-        "explanation"
+        "explanation",
     ]
 
     # Select only the required columns
     final_df = final_df[selected_columns]
+    final_df["explanation"] = final_df["explanation"].replace("\n", "\\n", regex=True)
     final_df.to_csv("eval_result.csv", index=False, encoding="utf-8-sig")
